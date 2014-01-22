266fbf1570f85df0ec79acd69b590f39e52e5160
Daniel Lipton <DanielLipton@yahoo.com>
2014-01-21 22:17:56 -0500

    added second round of nearness testing

--- src/com/daniellipton/javamltest/PointRemovalTest.java
+++ src/com/daniellipton/javamltest/PointRemovalTest.java
@@ -25,10 +25,10 @@ public class PointRemovalTest
 	
 	public static void main( String[] args )
 	{
-		int WIDTH = 1920/3, HEIGHT = 1080/3;
-		int POINTS = 2000 ;
-		int DIST = 10 ;
-		int CLUSTERS = 15 ;
+		int WIDTH = 1920/3, HEIGHT = 1080/3 ;
+		int POINTS = 5000 ; // points to draw on the screen
+		int DIST = 30 ; // minimum distance between points
+		int CLUSTERS = 10 ; // clusters to distribute the data
 		new PointRemovalTest( WIDTH, HEIGHT, POINTS, DIST, CLUSTERS ) ;
 	}
 	
@@ -41,28 +41,67 @@ public class PointRemovalTest
 //		Clusterer clusterer = new SOM( ) ;
 		nf.setMinimumFractionDigits( 4 ) ;
 		
-		JFrame frame = new JFrame( "Clustering Demo" ) ;
-		frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-		frame.setMinimumSize( new Dimension( width, height ) );
-		frame.setVisible( true ) ;
-		Cursor c = frame.getCursor( ) ;
-		frame.setCursor( Cursor.getPredefinedCursor( Cursor.WAIT_CURSOR ) );
+		// set up window frames
+		JFrame allDataFrame = new JFrame( "All Data" ) ;
+		allDataFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		allDataFrame.setMinimumSize( new Dimension( width, height ) );
+		JFrame shownDataFrame = new JFrame( "Shown Data" ) ;
+		shownDataFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		shownDataFrame.setMinimumSize( new Dimension( width, height ) );
+		JFrame calcDataFrame = new JFrame( "Calculated Data" ) ;
+		calcDataFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		calcDataFrame.setMinimumSize( new Dimension( width, height ) );
+		calcDataFrame.setLocation( allDataFrame.getLocation( ).x + width + 10, allDataFrame.getLocation( ).y );
+		shownDataFrame.setLocation( allDataFrame.getLocation( ).x, allDataFrame.getLocation( ).y + height + 10 );
+		calcDataFrame.setVisible( true ) ;
+		allDataFrame.setVisible( true ) ;
+		shownDataFrame.setVisible( true );
+		Cursor c = allDataFrame.getCursor( ) ;
+		allDataFrame.setCursor( Cursor.getPredefinedCursor( Cursor.WAIT_CURSOR ) ) ;
+		calcDataFrame.setCursor( Cursor.getPredefinedCursor( Cursor.WAIT_CURSOR ) ) ;
+		shownDataFrame.setCursor( Cursor.getPredefinedCursor( Cursor.WAIT_CURSOR ) ) ;
+		
+		// calculate random data points
 		System.out.println( "calculating data..." );
 		for( int i = 0 ; i < points ; i++ )
-		{
 			addData( dataset, width, height ) ;
-//			cluster( clusterer, dataset ) ;
-		}
+		
+		// find points to remove
 		System.out.println( "clustering..." );
 		Dataset[] data = cluster( clusterer, dataset ) ;
 		Dataset[] removeData = findClosestPoints( data, distance ) ;
+		// one more time on the remaining data w/o clusters
+		// first create the set of all data not removed
+		Dataset showData = new DefaultDataset( ) ;
+		Iterator<Instance> iter = dataset.iterator( ) ;
+		while( iter.hasNext( ) )
+		{
+			Instance d = iter.next( ) ;
+			boolean remove = false ;
+			for( int i = 0 ; i < removeData.length ; i++ )
+			{
+				if( removeData[i].contains( d ) )
+					remove = true ;
+			}
+			if( !remove )
+				showData.add( d.copy( ) ) ;
+		}
+		Dataset[] removeData2 = findClosestPoints( new Dataset[]{ showData }, distance ) ;
+		Dataset[] totalRemoveData = new Dataset[removeData.length + removeData2.length] ;
+		System.arraycopy( removeData, 0, totalRemoveData, 0, removeData.length ) ;
+		System.arraycopy( removeData2, 0, totalRemoveData, removeData.length, removeData2.length ) ;
+		
+		// draw all of the data
 		System.out.println( "drawing..." ) ;
-		draw( frame, dataset, data, removeData ) ;
-		frame.setCursor( c ) ; // done
+		draw( calcDataFrame, allDataFrame, shownDataFrame, dataset, data, totalRemoveData ) ;
+		shownDataFrame.setCursor( c ) ;
+		calcDataFrame.setCursor( c ) ; 
+		allDataFrame.setCursor( c ) ; // done
 	}
 	
 	/**
-	 * For each dataset, find the points within each cluster that are within a certain distance
+	 * For each dataset, find the points within each cluster that are within a certain distance.<p>
+	 * Brute-force approach!
 	 * 
 	 * @param datasets the clusters
 	 * @param	the minimum distance to use as a threshold
@@ -70,6 +109,7 @@ public class PointRemovalTest
 	 */
 	public Dataset[] findClosestPoints( Dataset[] datasets, int closest )
 	{
+		int pointsRemoved = 0 ;
 		Dataset[] remove = new Dataset[datasets.length] ;
 		long t1 = System.currentTimeMillis( ) ;
 		for( int i = 0 ; i < datasets.length ; i++ )
@@ -94,7 +134,10 @@ public class PointRemovalTest
 									Math.pow( x2 - x1, 2 ) +
 									Math.pow( y2 - y1, 2 ) ) ;
 							if( d <= closest )
+							{
 								remove[i].add( inst2.copy( ) ) ;
+								pointsRemoved++ ;
+							}
 //							System.out.println( "distance between ("+x1+","+y1+
 //									") & ("+x2+","+y2+")="+d );
 						}
@@ -104,7 +147,8 @@ public class PointRemovalTest
 //			System.out.println( remove[i] );
 		}
 		double t2 = ( System.currentTimeMillis( ) - t1 ) / 1000d ;
-		System.out.println( "finding closest points took " + nf.format( t2 ) + " secs" ) ;
+		System.out.println( "finding points to remove took " + nf.format( t2 ) + " secs" ) ;
+		System.out.println( "can remove " + pointsRemoved + " points" ) ;
 		return remove ;
 	}
 	
@@ -129,58 +173,88 @@ public class PointRemovalTest
 	/**
 	 * Draws the given data
 	 * 
-	 * @param frame make sure to set the size and make visible outside of this method
+	 * @param allDataFrame make sure to set the size and make visible outside of this method
 	 * @param allData	All of the data points
 	 * @param clusterData	All of the sets of clusters
 	 * @param removeData	The data points to be "removed"
 	 */
 	private void draw( 
-			JFrame frame, 
+			JFrame calcDataFrame,
+			JFrame allDataFrame,
+			JFrame shownDataFrame,
 			Dataset allData, 
 			Dataset[] clusterData,
 			Dataset[] removeData )
 	{
 		FastConvexHull hullExecutor = new FastConvexHull( ) ;
-		Graphics g = frame.getGraphics( ) ;
-		Font f = g.getFont( ).deriveFont( 8.0f ) ;
-		g.setFont( f ) ;
+		Graphics allGraphics = allDataFrame.getGraphics( ) ; // show all data points
+		Font f = allGraphics.getFont( ).deriveFont( 8.0f ) ;
+		allGraphics.setFont( f ) ;
+		Graphics shownGraphics = shownDataFrame.getGraphics( ) ; // show w/o removed data
+		f = shownGraphics.getFont( ).deriveFont( 8.0f ) ;
+		shownGraphics.setFont( f ) ;
+		Graphics calcGraphics = calcDataFrame.getGraphics( ) ; // show all data w/ clusters & highlighted removals
+		f = calcGraphics.getFont( ).deriveFont( 8.0f ) ;
+		calcGraphics.setFont( f ) ;
 		
 		if( allData != null )
 		{
-			g.setColor( Color.BLACK ) ;
-			draw( g, allData, "x", false ) ;
+			calcGraphics.setColor( Color.BLACK ) ;
+			allGraphics.setColor( Color.BLACK ) ;
+			shownGraphics.setColor( Color.BLACK ) ;
+//			draw( g, allData, "x", false ) ;
+			Iterator<Instance> iter = allData.listIterator( ) ;
+			while( iter.hasNext( ) )
+			{
+				Instance inst = iter.next( ) ;
+				int x = inst.get( 0 ).intValue( ) ;
+				int y = inst.get( 1 ).intValue( ) ;
+				calcGraphics.drawString( "x", x, y ) ;
+				allGraphics.drawString( "x", x, y ) ;
+				
+				// was this datapoint removed? if not then draw it on shownGraphics
+				boolean removed = false ;
+				for( int i = 0 ; i < removeData.length ; i++ )
+					if( removeData[i].contains( inst ) ) removed = true ;
+				
+				if( !removed )
+					shownGraphics.drawString( "x", x, y ) ;
+			}
 		}
 		
 		if( clusterData != null )
 		{
-			g.setColor( Color.RED ) ;
+			calcGraphics.setColor( Color.RED ) ;
 			for( int i = 0 ; i < clusterData.length ; i++ )
 			{
 //				draw( g, clusterData[i], "x", false ) ;
 				
 				// draw the hull
-				ArrayList<Point> points = new ArrayList<Point>( clusterData[i].size( ) ) ;
-				for( int j = 0 ; j < clusterData[i].size( ) ; j++ )
-					points.add( new Point( clusterData[i].get( j ).get( 0 ).intValue( ), 
-							clusterData[i].get( j ).get(  1 ).intValue( ) ) ) ;
-				ArrayList<Point> hull = hullExecutor.execute( points ) ;
-				Point last = hull.get( 0 ) ;
-				Point first = last ;
-				for( int j = 1 ; j < hull.size( ) ; j++ )
+				if( clusterData[i].size( ) > 1 )
 				{
-					Point p = hull.get( j ) ;
-					g.drawLine( last.x, last.y, p.x, p.y ) ;
-					last = p ; 
+					ArrayList<Point> points = new ArrayList<Point>( clusterData[i].size( ) ) ;
+					for( int j = 0 ; j < clusterData[i].size( ) ; j++ )
+						points.add( new Point( clusterData[i].get( j ).get( 0 ).intValue( ), 
+								clusterData[i].get( j ).get(  1 ).intValue( ) ) ) ;
+					ArrayList<Point> hull = hullExecutor.execute( points ) ;
+					Point last = hull.get( 0 ) ;
+					Point first = last ;
+					for( int j = 1 ; j < hull.size( ) ; j++ )
+					{
+						Point p = hull.get( j ) ;
+						calcGraphics.drawLine( last.x, last.y, p.x, p.y ) ;
+						last = p ; 
+					}
+					calcGraphics.drawLine( last.x, last.y, first.x, first.y ) ;
 				}
-				g.drawLine( last.x, last.y, first.x, first.y ) ;
 			}
 		}
 		
 		if( removeData!= null )
 		{
-			g.setColor( Color.BLUE ) ;
+			calcGraphics.setColor( Color.BLUE ) ;
 			for( int i = 0 ; i < removeData.length ; i++ )
-				draw( g, removeData[i], "x", false ) ;
+				draw( calcGraphics, removeData[i], "x", false ) ;
 		}
 	}
 	
